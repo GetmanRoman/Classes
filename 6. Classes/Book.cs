77a7cab3be2f43237f089e65ca5d025661d9754a
GetmanRoman <98706741+GetmanRoman@users.noreply.github.com>
2026-08-23 17:37:04 +0500

    Initial commit

---
+++ 6. Classes/Book.cs
@@ -0,0 +1,25 @@
+﻿namespace _6._Classes;
+
+public class Book
+{
+    private string Title; // Название
+    private string Author; // Автор
+    private int Year; // Год создания книги
+    private bool IsAvaliable; // Доступна ли к продаже
+
+    public Book()
+    {
+        Title = "";
+        Author = "";
+        Year = 0;
+        IsAvaliable = false;
+    }
+    
+    public Book(string title, string author, int year)
+    {
+        Title = title;
+        Author = author;
+        Year = year;
+        IsAvaliable = true;
+    }
+}
